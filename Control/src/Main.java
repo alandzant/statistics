bec546dd8725cb0c8ea18b58294b958fd46e42e7
Alan Dzant <alan.dzant@yandex.ru>
2023-03-16 00:17:28 +0300

    int change long

--- Control/src/Main.java
+++ Control/src/Main.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Main {
-}
+public class Main {
+        public static void main(String[] ars) {
+            long[] income  = new long[] {100, 50, 125, 356, 200, 500, 25};
+            SalesManager salesManager = new SalesManager(income);
+            System.out.println(salesManager.max());
+        }
+    }
--- Control/src/SalesManager.java
+++ Control/src/SalesManager.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class SalesManager {
+public class SalesManager {
+    protected long[] sales;
+
+     public SalesManager(long[] sales) {
+        this.sales = sales;
+    }
+
+    public long max() {
+        long max = -1;
+         for (long sale : sales) {
+             if (sale > max) {
+                    max = sale;
+            }
+         }
+         return max;
+     }
 }
+
