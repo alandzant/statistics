eaa17b677a11226e7e2d7a44c5b6eb62bd13b3cf
Alan Dzant <alan.dzant@yandex.ru>
2023-03-16 00:43:57 +0300

    Add class min and cropped_average

--- Control/src/Main.java
+++ Control/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
         public static void main(String[] ars) {
-            long[] income  = new long[] {100, 50, 125, 356, 200, 500, 25};
+            int[] income  = new int[] {100, 50, 125, 356, 200, 500, 25};
             SalesManager salesManager = new SalesManager(income);
+            System.out.println(salesManager.min());
             System.out.println(salesManager.max());
+            System.out.println(salesManager.cropped_average());
         }
     }
