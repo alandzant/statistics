caed35daa6821ae9b9f115773222f82cfc9c411d
Alan Dzant <alan.dzant@yandex.ru>
2023-03-16 01:06:45 +0300

    conflict fixed

--- Control/src/Main.java
+++ Control/src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
         public static void main(String[] ars) {
-            int[] income  = new int[] {100, 50, 125, 356, 200, 500, 25};
+            long[] income  = new long[] {100, 50, 125, 356, 200, 500, 25};
             SalesManager salesManager = new SalesManager(income);
-            System.out.println(salesManager.min());
-            System.out.println(salesManager.max());
             System.out.println(salesManager.cropped_average());
         }
     }
